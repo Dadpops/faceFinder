69b567a4825b8407daab7497f24c13a493ae6431
Chris Cabena <chris.cabena65@gmail.com>
2020-11-18 17:54:48 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation /> 
+      {/* {<Logo />
+      <ImageLink />
+      <FaceRecognition />} */}
+    </div>
+  );
+}
+
+export default App;
