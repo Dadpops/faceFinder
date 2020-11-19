a6b66e021bc7c8d4c467f9fffc26e331909f57d6
Chris Cabena <chris.cabena65@gmail.com>
2020-11-19 16:18:30 -0700

    Added particles.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLink from './components/ImageLink/ImageLink';
 import Rank from './components/Rank/Rank';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 1000,
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particlesOptions} />
       <Navigation /> 
       <Logo />
       <Rank />
